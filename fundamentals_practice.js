6203aad57d9601a510c9a4e8b0132539aae0e18f
Atikah <din.nuratikah@gmail.com>
2026-03-03 11:17:20 +0800

    This is for fundamentals practice for javascript. It includes variable declarations, simulated test steps, and an assertion to check if the actual title matches the expected title. The code demonstrates basic JavaScript concepts such as variables, arrays, objects, and conditional statements.

---
+++ fundamentals_practice.js
@@ -0,0 +1,22 @@
+let name = "Nuratikah";
+let age = 30;
+let isQAEngineer = true;
+let automationTools = ["Selenium", "Cypress", "Playwright"];
+let credentials = {
+  username: "nuratikah@example.com",
+  password: "abc123"
+};
+let expectedAge = 18;
+
+// Simulated test steps
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Is QA Engineer: " + isQAEngineer);
+console.log("Automation Tools: " + automationTools.join(", "));
+console.log("Username: " + credentials.username);
+console.log("Password: " + credentials.password);
+console.log(age >= expectedAge);
+console.log(age >= expectedAge && isQAEngineer)
+
+
+
